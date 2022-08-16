2144b167be044cf0c83c87cd011f1c9ad5121d36
Lucas Matheus Nunes Pereira <Lucasmathues416@outlook.com>
2022-08-16 15:15:02 -0300

    credit card done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./styles/App.css";
+import CreditCard from "./components/CreditCard";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const expireDate = {
+    month: 12,
+    year: 14,
+  };
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="app">
+      <CreditCard
+        name="Lucas Matheus"
+        number="1234567891011135"
+        date={expireDate}
+        cvc="123"
+        side={false}
+      />
+      <CreditCard cvc="123" side={true} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/CreditCard/index.jsx
@@ -0,0 +1,43 @@
+import "./CreditCard.css";
+
+function CreditCard({ name, number, date, cvc, side }) {
+  const cardLogo = "./images/card-logo.svg";
+  const isSide = side ? "back" : "front";
+  const background = side
+    ? "./images/bg-card-back.png"
+    : "./images/bg-card-front.png";
+
+  return (
+    <div
+      className={`card ${isSide}`}
+      style={{
+        backgroundImage: `url(${background})`,
+      }}
+    >
+      {side ? (
+        <div className="wrapper">
+          <h1 className="cvc">{cvc}</h1>
+        </div>
+      ) : (
+        <>
+          <img src={cardLogo} alt="card icon" />
+          <section className="card__details">
+            <h1 className="details__number">
+              {number.split("").map((e) => (
+                <span key={Math.random() * parseInt(e)}>{e}</span>
+              ))}
+            </h1>
+            <footer>
+              <h1>{name}</h1>
+              <small>
+                {date.month}/{date.year}
+              </small>
+            </footer>
+          </section>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default CreditCard;
